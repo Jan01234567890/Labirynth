8d6f1a6c82bccb2e76a9c6e5e95af7404c18745c
Jan01234567890 <janek.appinventor@gmail.com>
2022-02-26 13:31:02 +0100

    Modify GameManager, PlayerController

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager gameManager;
+    
+    bool gamePaused = false;
+    bool endGame = false;
+    bool win = false;
+    public int timeToEnd;
+
     void Start()
     {
-        
+        if (gameManager== null)
+        {
+            gameManager = this;
+        }
+
+        if(timeToEnd <= 0)
+        {
+            timeToEnd = 100;
+        }
+
+        Debug.Log("Time: " + timeToEnd + " s");
+        InvokeRepeating("Stopper", 2, 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Debug.LogWarning("Pause Game");
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume Game");
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Stopper()
+    {
+        timeToEnd--;
+        Debug.Log("Time: " + timeToEnd + " s");
+
+        if(endGame)
+        {
+            return;
+        }
+
+        if(timeToEnd<=0)
+        {
+            timeToEnd = 0;
+            endGame = true;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed = 12f;
+    float gravity = -10;
+    Vector3 velocity;
     CharacterController characterController;
+
+    public Transform groundCheck;
+    public LayerMask groungMask;
 
     void Start()
     {
@@ -16,16 +21,42 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
+
+        CheckGround();
     }
 
     void PlayerMove()
     {
-        // -1, gdy idziemy w lewo; 1, gdy idziem w prawo
+        // -1, gdy idziemy w lewo; 1, gdy idziemy w prawo
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         //Vector3 move = Vector3.right * x + Vector3.forward * z;
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groungMask))
+        {
+            string terrainType = hit.collider.gameObject.tag;
+
+            switch(terrainType)
+            {
+                default:
+                    speed = 12f;
+                    break;
+                case "Low":
+                    speed = 4f;
+                    break;
+                case "High":
+                    speed = 20f;
+                    break;
+            }
+        }
     }
 }
