51f251fa3e0356bcf2646ecc795f7f00c0d03ebb
Jan01234567890 <janek.appinventor@gmail.com>
2022-04-23 13:21:40 +0200

    Lock

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Transform doorT;
+    public Transform openPosition;
+    public Transform closePosition;
+
+    public float speed = 1f;
+    public bool open = false;
+
+    private void Update()
+    {
+        if(open)
+        {
+            doorT.position = Vector3.MoveTowards(doorT.position, openPosition.position, Time.deltaTime * speed);
+        }
+
+        if (!open)
+        {
+            doorT.position = Vector3.MoveTowards(doorT.position, closePosition.position, Time.deltaTime * speed);
+        }
+    }
+
+    public void CloseOpen()
+    {
+
+    }
+}
